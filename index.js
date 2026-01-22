e8216b013092ad10be6b885b2e1c30aed1cf0915
ndrijephte <ndrijephtekoffi@gmail.com>
2026-01-22 11:35:17 +0000

    Projet terminé avec optimisation des codes JS et CSS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const countriesContainer = document.querySelector(".countries-container");
+const btnSort = document.querySelectorAll(".btnSort");
 let countriesData = [];
+let sortMethod = "maxToMin";
 
 // 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
 async function fetchCountries() {
@@ -10,8 +12,26 @@ async function fetchCountries() {
   countriesDisplay();
 }
 
+// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+// 3 - Passer les données à une variable (pour ce faire créer la variable)
+// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 function countriesDisplay() {
   countriesContainer.innerHTML = countriesData
+    .filter((country) =>
+      country.translations.fr
+        .toLowerCase()
+        .includes(inputSearch.value.toLowerCase()),
+    )
+    .sort((a, b) => {
+      if (sortMethod === "maxToMin") {
+        return b.population - a.population;
+      } else if (sortMethod === "minToMax") {
+        return a.population - b.population;
+      } else if (sortMethod === "alpha") {
+        return a.translations.fr.localeCompare(b.translations.fr);
+      }
+    })
+    .slice(0, inputRange.value)
     .map(
       (country) => `
         <div class="card">
@@ -24,18 +44,23 @@ function countriesDisplay() {
     )
     .join("");
 }
-
 window.addEventListener("load", fetchCountries);
 
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
-
-// 3 - Passer les données à une variable (pour ce faire créer la variable)
-
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
-
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-// coutry.name.includes(inputSearch.value);
+
+// Pour exécuter le filtre
+inputSearch.addEventListener("input", countriesDisplay);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
+inputRange.addEventListener("input", () => {
+  countriesDisplay();
+  rangeValue.textContent = inputRange.value;
+});
 
 // 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
+btnSort.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    sortMethod = e.target.id;
+    countriesDisplay();
+  });
+});
